7ee77495408fdfc4fc188d7bddc904cf2db4f023
novakoda <novakoda9@gmail.com>
2020-04-24 18:51:37 -0500

    created members and migrated db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :members
   resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "messages#index"
+
 end
